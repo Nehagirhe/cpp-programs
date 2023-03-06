0c33a0c76038b28e09c33a7e176234595ad610d9
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-06 14:01:31 +0530

    Add files via upload

---
+++ Programs/character.cpp
@@ -0,0 +1,16 @@
+//Program Which Accepts A Character And Display Its Next Character.
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+ 	char ch;
+ 	cout<<"Enter Any Character : ";
+ 	cin>>ch;
+ 	cout<<"\nNext Character Is : ";
+ 	ch++;
+ 	cout<<ch;
+ 	
+	return 0;
+}
