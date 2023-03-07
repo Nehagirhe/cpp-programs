a25b1610ea17ca6ecbc0629b75c4758bcf805bc3
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-07 13:17:59 +0530

    Add files via upload

---
+++ Programs/marks.cpp
@@ -0,0 +1,14 @@
+/*Program Ti\o Display The Following Output Using A Single Cout Statement.
+ Subject         Marks
+ Mathematics       90
+ Comouter          77
+ Chemistry         69 */
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+cout<<"Subject         "<<"Marks "<<"\nMathematics     "<<"90    "<<"\nComputer        "<<"77    "<<"\nChemistry       "<<"69    ";
+	
+	return 0;
+}
