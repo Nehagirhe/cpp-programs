366c958845c88b818f310974a4858c3b6d5f3047
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-08 08:37:56 +0530

    Add files via upload

---
+++ Inheritance/hierachical.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+class A
+{
+	public:
+		int l,b;
+		void AcceptValue()
+		{
+			cout<<"\n\nEnter Length : ";
+			cin>>l;
+			cout<<"Enter Breadth : ";
+			cin>>b;
+		}
+		
+};
+class B:public A
+{
+	public:
+		void AreaRect()
+		{
+			cout<<"Area = "<<l*b;
+			
+		}
+};
+
+class C : public A
+{
+	public:
+		void Perimeter()
+		{
+			cout<<"Perimeter Of Rectangle : "<<2*(l+b)<<endl<<endl;
+			
+		}
+};
+
+int main()
+ {
+ B b;
+ C c;
+ cout<<"\n\n**********  FOR AREA OF RECTANGLE   *************";
+ b.AcceptValue();
+ b.AreaRect();
+ cout<<"\n\n\n*********   FOR PERIMETER OF RECTANGLE   ********";
+ c.AcceptValue();
+ c.Perimeter();	
+	return 0;
+}
