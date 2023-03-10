87a0698d781393babc62fb5d2c7693a4136dae5d
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-10 10:28:40 +0530

    Update amount.cpp

--- Constructor/amount.cpp
+++ Constructor/amount.cpp
@@ -1,3 +1,9 @@
+/*Suppose You Have A Piggie Bank With An Initial Amount of $50 And You Have To Add Some More Amount To It. Create A Class 'AddAmount'
+With A Data Member Named 'amount' With An Initial value Of $50. Now Make Two Constructors Of This Class As Follows:
+1- Without Any Parameter - no Amount Will Be Added To the Piggie Bank.
+2-having A Parameter Which Is The Amount That Will Be Added To The Piggie Bank
+Create An Object Of The  'AddAmount' Class And Display The Final Amount In The Piggie Bank.*/
+
 #include <iostream>
 using namespace std;
 class AddAmount
