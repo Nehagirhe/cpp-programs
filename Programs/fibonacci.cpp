e00d13e1438b89d215bb989fbd1abb4b68eac9d9
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-11 06:51:11 +0530

    Update fibonacci.cpp

--- Programs/fibonacci.cpp
+++ Programs/fibonacci.cpp
@@ -11,7 +11,7 @@ int main() {
     cout << "Fibonacci Series: ";
 
     for (int i = 1; i <= n; ++i) {
-        // Prints the first two terms.
+        
         if(i == 1) {
             cout << t1;
             continue;
