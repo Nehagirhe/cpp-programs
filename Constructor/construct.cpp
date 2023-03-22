fb3acc26976b56ca29035767ee5efa5ed25eaa2c
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-22 13:56:48 +0530

    Add files via upload

---
+++ Constructor/construct.cpp
@@ -0,0 +1,35 @@
+// defining the constructor within the class
+ 
+#include<iostream>
+using namespace std;
+class student
+{
+    int rno;
+    char name[50];
+    double fee;
+    public:
+    student()
+    {
+        cout<<"Enter the RollNo:";
+        cin>>rno;
+        cout<<"Enter the Name:";
+        cin>>name;
+        cout<<"Enter the Fee:";   
+        cin>>fee;
+    }   
+     
+ 
+ 
+    void display()
+    {
+        cout<<endl<<rno<<"\t"<<name<<"\t"<<fee;
+    }
+};
+ 
+int main()
+{
+    student s;  
+    s.display();
+    return 0;
+ 
+}
