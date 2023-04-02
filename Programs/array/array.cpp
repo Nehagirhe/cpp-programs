f3cadb5fd92a5bd41414e4cd89ebea6a3c66e5ec
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-04-02 07:23:07 +0530

    Add files via upload

---
+++ Programs/array/array.cpp
@@ -0,0 +1,27 @@
+// Passing One-dimensional Array to a Function
+// C++ Program to display marks of 5 students
+
+#include <iostream>
+using namespace std;
+
+
+void display(int m[5]) {
+    cout << "Displaying marks: " << endl;
+
+    // display array elements    
+    for (int i = 0; i < 5; ++i) {
+        cout << "Student " << i + 1 << ": " << m[i] << endl;
+    }
+}
+
+int main() {
+
+    // declare and initialize an array
+    int marks[5] = {88, 76, 90, 61, 69};
+    
+    // call display function
+    // pass array as argument
+    display(marks);
+
+    return 0;
+}
