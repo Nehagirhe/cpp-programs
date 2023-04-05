18a1ea69a75e4e8880695d667d87724d4553e2ce
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-04-05 09:58:20 +0530

    Update array.cpp

--- Programs/array/array.cpp
+++ Programs/array/array.cpp
@@ -18,9 +18,7 @@ int main() {
 
     // declare and initialize an array
     int marks[5] = {88, 76, 90, 61, 69};
-    
-    
-    // pass array as argument
+   
     display(marks);
 
     return 0;
